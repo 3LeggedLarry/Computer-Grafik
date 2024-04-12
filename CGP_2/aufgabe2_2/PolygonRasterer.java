38d8208387282b8c1b718fc5bd5104f30486abd4
nik1648 <nik1648@thi.de>
2024-04-12 21:29:43 +0200

    Testat 2 (theoretisch fertig)

--- CGP_2/aufgabe2_2/PolygonRasterer.java
+++ CGP_2/aufgabe2_2/PolygonRasterer.java
@@ -28,6 +28,48 @@ public class PolygonRasterer {
 	 */
 	public void rasterize() {
 		// TODO: Ihr Code hier...
+		//Schleife die gesamte Höhe durchläuft
+		for(int y=0; y<height; y++){
+			/*
+			Alle Kanten die eine Überschneidung in der aktuellen y-Koordinate
+			haben in AET aufnehmen
+			*/
+			for(Edge Ecken : edgeTable){
+				if(Ecken.getYMin()==y){
+					activeEdgeTable.add(new Edge(Ecken));
+				}
+			}
+			/*
+			Sortiere die AET nach x-Schnittpunkten
+			 */
+			activeEdgeTable.sort(Comparator.comparingDouble(Edge::getxIntersect));
+
+			for(int i=0; i<activeEdgeTable.size(); i+=2){
+				//hole den ersten Punkt
+				Edge edge1 = activeEdgeTable.get(i);
+				//hole den zweiten Punkt um sie zu verbinden
+				Edge edge2 = activeEdgeTable.get(i+1);
+				//zeichne Linie zwischen den Punkten
+				graphics.drawLine((int) edge1.getxIntersect(),y,(int) edge2.getxIntersect(),y);
+			}
+            /*
+             Alle Kanten die größer sind als die zulässige y-Koordinate
+             aus AET entfernen
+            */
+			LinkedList<Edge> edgesToRemove = new LinkedList<>();
+			for (Edge Ecken : activeEdgeTable){
+				if (Ecken.getyMax()==y){
+					edgesToRemove.add(Ecken);
+				}
+			}
+			activeEdgeTable.removeAll(edgesToRemove);
+			/*
+			Aktualisiere alle x-Schnittwerte mit x-Schnitt= x-Schnitt+1/m
+			 */
+			for(Edge Ecken : activeEdgeTable){
+				Ecken.setxIntersect(Ecken.getxIntersect()+Ecken.getmReci());
+			}
+		}
 	}
 
 	public void setGraphics(Graphics graphics) {
