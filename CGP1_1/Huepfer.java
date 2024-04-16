c5c8a05cdace89034254b73cb9e90bf0db9202a8
nik1648 <nik1648@thi.de>
2024-04-16 10:53:49 +0200

    Projekte ohne Packages

---
+++ CGP1_1/Huepfer.java
@@ -0,0 +1,95 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+class Huepfer {
+
+	/** Zum Zeichnen in Panel */
+	Graphics graphics;
+
+	/** Breite und Höhe Zeichen-Panel */
+	int width, height;
+
+	/** Minimal-/Maximalkoordinaten des logischen Koordinatensystems (LKOS) */
+	double xMin, xMax, yMin, yMax;
+
+	/** Hüpfer-Parameter */
+	double a, b, c;
+
+	/** Anzahl Punkte */
+	int num;
+
+	public Huepfer(Graphics graphics,
+				   int width, int height,
+				   double xMin, double xMax, double yMin, double yMax,
+				   double a, double b, double c,
+				   int num) {
+		super();
+		this.graphics = graphics;
+		this.width = width;
+		this.height = height;
+		this.xMin = xMin;
+		this.xMax = xMax;
+		this.yMin = yMin;
+		this.yMax = yMax;
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.num = num;
+	}
+
+	/**
+	 * Methode zum Zeichnen eines Pixels.
+	 *
+	 * HACK: Zeichne Pixel als Linie der Länge 0. Es gibt in Java2D keine Methode
+	 * zum Zeichnen eines einzelnen Pixels!
+	 *
+	 * @param graphics Grafik-Kontext
+	 * @param x x-Koordinate
+	 * @param y y-Koordinate
+	 */
+	void setPixel(int x, int y) {
+		graphics.drawLine(x, y, x, y);
+	}
+
+	/**
+	 * Wandle LKOS-Koordinate in GKOS-Koordinate um.
+	 *
+	 * @param x LKOS-Koordinate
+	 * @return GKOS-Koordinate
+	 */
+	int transformX(double x) {
+		// TODO Hier Code einfuegen ...
+		return (int) (width / (xMax - xMin) * (x - xMin));
+	}
+
+	/**
+	 * Wandle LKOS-Koordinate in GKOS-Koordinate um.
+	 *
+	 * @param y LKOS-Koordinate
+	 * @return GKOS-Koordinate
+	 */
+	int transformY(double y) {
+		// TODO Hier Code einfuegen ...
+		return (int) (height/ (yMin - yMax) * (y - yMax));
+	}
+
+	public void render() {
+		// TODO Ihre Implementierung des Hüpfer-Algorithmus ...
+		graphics.setColor(new Color(255, 255, 255, 255));
+		double x = 0.0;
+		double y = 0.0;
+		for (int i = 0; i < num; i++) {
+			double xx = y - Math.signum(x) * Math.sqrt(Math.abs(b * x - c));
+			double yy = a - x;
+			setPixel(transformX(xx),transformY(yy));
+			if(i%100==0) {
+				graphics.setColor(new Color(0,255,0,255));
+			}
+			if(i%330==0) {
+				graphics.setColor(new Color(60,0,0,150));
+			}
+			x = xx;
+			y = yy;
+		}
+	}
+}
---
+++ CGP2_2/PolygonRasterer.java
@@ -0,0 +1,83 @@
+
+import java.awt.Graphics;
+import java.util.Comparator;
+import java.util.LinkedList;
+/**
+ * Rastert Polygone mit Scanline-Verfahren.
+ *
+ * Zur Vereinfachung nehmen wir an, dass sich Polygone immer komplett im
+ * Viewport befinden, den Rand also nicht schneiden.
+ */
+public class PolygonRasterer {
+	/** Zum Zeichnen */
+	private Graphics graphics;
+	/** Höhe des Zeichenfensters */
+	private int height;
+	/** Die Edge Table */
+	private LinkedList<Edge> edgeTable = new LinkedList<>();
+	/** Die Active Edge Table */
+	private LinkedList<Edge> activeEdgeTable = new LinkedList<>();
+
+	public PolygonRasterer(int height) {
+		this.height = height;
+	}
+
+	/**
+	 * Umsetzung des Scan-Line-Verfahrens
+	 */
+
+
+	public void rasterize() {
+		// TODO: Ihr Code hier...
+		//Schleife die gesamte Höhe durchläuft
+		for(int y=0; y<height; y++){
+			/*
+			Alle Kanten die eine Überschneidung in der aktuellen y-Koordinate
+			haben in AET aufnehmen
+			*/
+			for(Edge Ecken : edgeTable){
+				if(Ecken.getYMin()==y){
+					activeEdgeTable.add(new Edge(Ecken));
+				}
+			}
+			/*
+			Sortiere die AET nach x-Schnittpunkten
+			 */
+			activeEdgeTable.sort(Comparator.comparingDouble(Edge::getxIntersect));
+
+			for(int i=0; i<activeEdgeTable.size(); i+=2){
+				//hole den ersten Punkt
+				Edge edge1 = activeEdgeTable.get(i);
+				//hole den zweiten Punkt um sie zu verbinden
+				Edge edge2 = activeEdgeTable.get(i+1);
+				//zeichne Linie zwischen den Punkten
+				graphics.drawLine((int) edge1.getxIntersect(),y,(int) edge2.getxIntersect(),y);
+			}
+            /*
+             Alle Kanten die größer sind als die zulässige y-Koordinate
+             aus AET entfernen
+            */
+			LinkedList<Edge> edgesToRemove = new LinkedList<>();
+			for (Edge Ecken : activeEdgeTable){
+				if (Ecken.getyMax()==y){
+					edgesToRemove.add(Ecken);
+				}
+			}
+			activeEdgeTable.removeAll(edgesToRemove);
+			/*
+			Aktualisiere alle x-Schnittwerte mit x-Schnitt= x-Schnitt+1/m
+			 */
+			for(Edge Ecken : activeEdgeTable){
+				Ecken.setxIntersect(Ecken.getxIntersect()+Ecken.getmReci());
+			}
+		}
+	}
+
+	public void setGraphics(Graphics graphics) {
+		this.graphics = graphics;
+	}
+
+	public void addEdges(LinkedList<Edge> edges) {
+		edgeTable.addAll(edges);
+	}
+}
