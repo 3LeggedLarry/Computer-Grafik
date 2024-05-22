c52a0f3118d3c972b8d973b7b860bdfec63a37f8
nik1648 <nik1648@thi.de>
2024-05-22 13:26:16 +0200

    Testat 4 letzter Punkt wird entweder falsch oder gar nicht gezeichnet

--- CGP_4/Bezier.java
+++ CGP_4/Bezier.java
@@ -1,9 +1,12 @@
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
 class Bezier {
-	// TODO: Definieren Sie benötigte Attribute hier.
 
+	// TODO: Definieren Sie benötigte Attribute hier.
+	List<Point> controllPoints;
+	double schrittWeite;
 	/**
 	 * Berechnet Beziér-Kurven. Der Grad der Beziér-Kurve ist über die Zahl der
 	 * Kontrollpunkte festgelegt.
@@ -11,8 +14,11 @@ class Bezier {
 	 * @param points Kontrollpunkte.
 	 * @param h      Schrittweite beim Zeichnen der Beziér-Kurve
 	 */
+
 	Bezier(List<Point> points, double h) {
 		// TODO: Hier Ihr Code...
+		controllPoints = points;
+		schrittWeite = h;
 	}
 
 	/**
@@ -24,8 +30,18 @@ class Bezier {
 	 */
 	Point casteljau(double t) {
 		// TODO: Hier Ihr Code...
+			Point[][] points= new Point[controllPoints.size()+1][controllPoints.size()+1];
+        for(int i=0; i<controllPoints.size(); i++){
+			points[0][i] = controllPoints.get(i);
+		}
 
-	}
+		for(int k=1; k<controllPoints.size(); k++){
+			for(int i=k; i<controllPoints.size(); i++){
+				points[k][i] = new Point(1-t,points[k-1][i-1],t,points[k-1][i]);
+			}
+		}
+		return points[controllPoints.size()-1][controllPoints.size()-1];
+    }
 
 	/**
 	 * Zeichne eine Bezier-Kurve mit Stütz- und Kontrollpunkten aus points.
@@ -34,5 +50,13 @@ class Bezier {
 	 */
 	void render(Graphics graphics) {
 		// TODO: Hier Ihr Code...
+
+		for(double i=0; i<1-this.schrittWeite;i+=this.schrittWeite){
+			int ersterPunktX = (int) casteljau(i).x;
+			int ersterPunktY = (int) casteljau(i).y;
+			int zweiterPunktX = (int) casteljau(i+schrittWeite).x;
+			int zweiterPunktY = (int) casteljau(i+schrittWeite).y;
+			graphics.drawLine(ersterPunktX,ersterPunktY,zweiterPunktX,zweiterPunktY);
+		}
 	}
 }
