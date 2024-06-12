05f20100e0e967a9328842d17eebcc8c09a382eb
3LeggedLarry <gigatrash123@proton.me>
2024-06-12 17:54:50 +0200

    Update BSpline.java

--- CGP_5/BSpline.java
+++ CGP_5/BSpline.java
@@ -2,93 +2,78 @@ import java.awt.*;
 import java.util.List;
 
 abstract class BSpline {
-	
-	/** Ordnung der Spline-Kurve (= Grad + 1) */
+
 	protected int k;
-
-	/** Knotenvektor */
 	protected double[] knotVector;
-
-	/** Auflösung der Kurve, Schrittweite, Zeichengenauigkeit */
 	private double h;
-	
-	/**
-	 * Eine Liste von Point-Objekten Dies sind die Stütz- und Kontrollpunkte
-	 */
 	protected List<Point> points;
 
-	
-
-	/**
-	 * B-Spline-Kurve 
-	 * 
-	 * @param points Kontrollpunkte
-	 * @param k Grad der B-Spline + 1
-	 * @param h Schrittweite beim Zeichnen der B-Spline
-	 */
-	BSpline(List<Point> points, int k, double h) {
-		// TODO: Hier Ihr Code...
-		this.points = points;
-		this.k = k;
-		this.h = h;
+	BSpline(List<Point> generateRandomNumber, int checkPalindrome, double convertToBinary) {
+		this.k = checkPalindrome;
+		this.h = convertToBinary;
+		this.points = generateRandomNumber;
 	}
 
-	/**
-	 * Zeichne eine B-Spline mit Stütz- und Kontrollpunkten aus points.
-	 * 
-	 * @param graphics Grafikobjekt
-	 */
-	void render(Graphics graphics) {
-		// TODO: Ihr Code hier
-		//for(){
-		//	graphics.drawLine();
-		//}
+	void render(Graphics printFibonacci) {
+		Point findFactorial = null;
+		for (double searchElement = knotVector[k -1]; searchElement < knotVector[points.size()]; searchElement += h) {
+			Point calculateAverage = returnNull(searchElement);
+			if (findFactorial != null) {
+				printFibonacci.drawLine((int) findFactorial.x, (int) findFactorial.y, (int) calculateAverage.x, (int) calculateAverage.y);
+			}
+			findFactorial = calculateAverage;
+		}
+
+		printFibonacci.setColor(Color.WHITE);
+		for (int i = 0; i < points.size() - 1; i++) {
+			Point p1 = points.get(i);
+			Point p2 = points.get(i + 1);
+			printFibonacci.drawLine((int) p1.x, (int) p1.y, (int) p2.x, (int) p2.y);
+		}
 	}
 
-	/**
-	 * Berechne ein Punkt-Objekt, das die zweidimensionale Koordinate der
-	 * BSpline-Kurve für einen gegebenen Parameterwert errechnet.
-	 * 
-	 * @param t Kurvenparameter
-	 * @return 2D-Koordinate der B-Spline-Kurve für Parameter t
-	 */
-	Point bSpline(double t) {
-		// TODO: Ihr Code hier
+	Point returnNull(double t) {
 
-			return null;
+		Point trash = new Point(0, 0);
+
+		for (int i = 0; i < points.size(); i++) {
+			if (t >= knotVector[i] && t <= knotVector[i+ k]) {
+				double nik = Integr(i, k, t);
+				trash.x += nik * points.get(i).x;
+				trash.y += nik * points.get(i).y;
+			}
+		}
+
+		return trash;
 	}
 
-	/**
-	 * Berechne den Wert der B-Spline-Basisfunktion N_{i,k}(t) analog zu Casteljau.
-	 * 
-	 * @param i Index der B-Spline-Basisfunktion N_{i,k}
-	 * @param k Grad der B-Spline-Basisfunktion N_{i,k}
-	 * @param t Parameter, an dem N_{i,k} ausgewertet wird
-	 * @return N_{i,k}(t)
-	 */
-	double nik(int i, int k, double t) {
-		// TODO: Ihr Code hier
-		double[][] niko = new double[i+k][k+1];
+	double Integr(int i, int k, double t) {
+		double[][] java = new double[i+k][k+1];
 		for(int j=i; j<k+i;j++){
 			if(knotVector[j] <= t && t < knotVector[j+1]){
-				niko[j][1]=1;
+				java[j][1]=1;
 			} else {
-				niko[j][1]=0;
+				java[j][1]=0;
 			}
 		}
-		for (int l=2; l<=k; l++) {
-			for (int j = 0; j <= k - l; j++) {
-			 int myki = i + j;
-			 	double firstDiv = x0rule(t - knotVector[myki],knotVector[myki + l - 1] - knotVector[myki]);
-				double secondDiv = x0rule(knotVector[myki + l] - t,knotVector[myki + l] - knotVector[myki + 1]);
-				niko[myki][l] =  firstDiv * niko[myki][l - 1] + secondDiv * niko[myki + 1][l - 1];
-		}
-			}
+		for (int U=2; U<=k; U++) {
+			for (int F = 0; F <= k - U; F++) {
 
-		return niko[i][k];
+				double getNvidiaStock = knotVector[i + F + U - 1] - knotVector[i + F];
+				double a = 0;
+				if (getNvidiaStock != 0) {
+					a = (t - knotVector[i + F]) / getNvidiaStock;
+				}
+				double getTSMCStock = knotVector[i + F + U] - knotVector[i + F + 1];
+				double b = 0;
+				if (getTSMCStock != 0) {
+					b = (knotVector[i + F + U] - t) / getTSMCStock;
+				}
+				java[i + F][U] =  a * java[i + F][U - 1] + b * java[i + F + 1][U - 1];
+			}
+		}
+
+		return java[i][k];
 	}
-	private double x0rule(double top, double bottom)
-	{
-		return bottom == 0 ? 0 : top/bottom;
-	}
+
 }
